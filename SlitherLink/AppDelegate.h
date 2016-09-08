7c36e2117d44e5426ac779fe0f226beab3b0c9ca
Rakesh Patole <rakeshjpatole@gmail.com>
2016-09-08 16:51:43 +0530

    basic Xcode project commit

---
+++ SlitherLink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlitherLink
+//
+//  Created by Rakesh Patole on 9/8/16.
+//  Copyright © 2016 Rakesh Patole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlitherLink/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlitherLink
+//
+//  Created by Rakesh Patole on 9/8/16.
+//  Copyright © 2016 Rakesh Patole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
