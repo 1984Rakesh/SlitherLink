a420905761936ab32ed6e516ed1aecc0224d85c9
Rakesh Patole <patolerakesh@gmail.com>
2016-09-13 11:19:28 +0530

    position grid in center

--- SlitherLink/SLGameView.m
+++ SlitherLink/SLGameView.m
@@ -14,16 +14,29 @@
 - (void) drawRect:(CGRect)rect {
     CGContextRef context = UIGraphicsGetCurrentContext();
     NSArray *edges = [[self grid] edges];
-    CGFloat lineLen = 30.0f;
+    CGFloat width = CGRectGetWidth(self.bounds);
+    CGFloat height = CGRectGetHeight(self.bounds);
+    CGFloat lineLen = 0.0f;
+    CGFloat margin = 20.0f;
+    if( width > height ){
+        lineLen = (height - margin) / [self.grid size].height;
+    }
+    else {
+        lineLen = (width - margin) / [self.grid size].width;
+    }
+    CGFloat startX = (width - lineLen * self.grid.size.width) / 2.0f;
+    CGFloat startY = (height - lineLen * self.grid.size.height) / 2.0f;
     [edges enumerateObjectsUsingBlock:^(SLGridFaceEdge *edge, NSUInteger idx, BOOL * _Nonnull stop) {
         SLGridFaceCorner *corner1 = [edge faceCorner1];
         SLGridFaceCorner *corner2 = [edge faceCorner2];
-        CGPoint p1 = CGPointMake( corner1.position.x * lineLen, corner1.position.y * lineLen);
-        CGPoint p2 = CGPointMake( corner2.position.x * lineLen, corner2.position.y * lineLen);
+        CGPoint p1 = CGPointMake((corner1.position.x * lineLen) + startX,
+                                 (corner1.position.y * lineLen) + startY);
+        CGPoint p2 = CGPointMake((corner2.position.x * lineLen) + startX,
+                                 (corner2.position.y * lineLen) + startY);
         [self drawLineWithP1:p1
                        andP2:p2
                    inContext:context
-                       color:[UIColor blackColor]];
+                       color:[UIColor lightGrayColor]];
     }];
     
     
